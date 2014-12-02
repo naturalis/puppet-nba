19e86c5afdfae636a885cf7cf87643b3d2939ec4
Atze de Vries <atze.devries@naturalis.nl>
2014-12-02 16:06:26 +0100

    updated to new es version

--- manifests/es.pp
+++ manifests/es.pp
@@ -3,7 +3,7 @@
 class nba::es (
   $nba_cluster_name   = 'changeme',
   $es_version         = '1.3.4',
-  $es_repo_version    = '1.3'
+  $es_repo_version    = '1.3',
   $shards             = 9,
   $replicas           = 1,
   $es_memory_gb       = 8,
