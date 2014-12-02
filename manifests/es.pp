b19e49dfc90c4877bcb94c37115e791619e826c9
Atze de Vries <atze.devries@naturalis.nl>
2014-12-02 15:51:25 +0100

    updated to new es version

--- manifests/es.pp
+++ manifests/es.pp
@@ -2,12 +2,12 @@
 #
 class nba::es (
   $nba_cluster_name   = 'changeme',
-  $es_version         = '1.3.2',
+  $es_version         = '1.3.4',
   $shards             = 9,
-  $replicas           = 0,
+  $replicas           = 1,
   $es_memory_gb       = 8,
-  $es_data_dir        = '/data/elasticsearch',
   $install_marvel     = false,
+  $install_kopf       = false,
   $snapshot_directory = '/data/snapshots',
   $mount_snapshot     = false,
   $snapshot_server    = '127.0.0.1',
@@ -15,46 +15,58 @@ class nba::es (
 
   if $nba_cluster_name == 'changeme' { fail('Change the variable nba_cluster_name to a propper one') }
 
-  class{ 'elasticsearch':
-    package_url               => "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${es_version}.deb",
-    config                    => {
-        'node'                  => {
-          'name'                => $::hostname
-        },
-        'index'                 => {
-          'number_of_shards'    => $shards,
-          'number_of_replicas'  => $replicas
-        },
-        'cluster'               => {
-          'name'                => $nba_cluster_name
-        }
-      },
-    java_install              => true,
-    init_defaults             => {
-        'ES_HEAP_SIZE'          => "${$es_memory_gb}g",
-        'DATA_DIR'              => $es_data_dir
-    },
-  }
+  $es_instance_name = 'nba-instance'
 
-  if $install_marvel {
-    exec { 'install marvel' :
-      command => '/usr/share/elasticsearch/bin/plugin -i elasticsearch/marvel/latest',
-      unless  => '/usr/bin/test -d /usr/share/elasticsearch/plugins/marvel',
+  class { 'elasticsearch':
+    version      => $es_version,
+    java_install => true
+    config       => {
+      'cluster.name'             => $nba_cluster_name,
+      'index.number_of_shards'   => $shards,
+      'index_number_of_replicas' => $replicas
     }
   }
 
-  package {'nfs-common':}
-
-  file {['/data',$snapshot_directory]:
-    ensure => directory,
-    mode   => '0777',
-  } ->
-
-  exec {"mount snapshot dir to ${snapshot_directory}":
-    command => "/bin/mount ${snapshot_server}:/data/snapshots ${snapshot_directory}",
-    unless  => "/bin/mount | grep ${snapshot_server} | grep ${snapshot_directory}",
-    require => Package['nfs-common']
+  elasticsearch::instance { $es_instance_name :
+    init_defaults => {
+      'ES_HEAP_SIZE' => "${es_memory_gb}g"
+    }
   }
 
+  if $install_kopf {
+    elasticsearch::plugin{'lmenezes/elasticsearch-kopf':
+      module_dir => 'kopf',
+      instances  => $es_instance_name
+    }
+  }
+  if $install_marvel {
+    elasticsearch::plugin{ 'elasticsearch/marvel/latest':
+      module_dir => 'marvel',
+      instances  => $es_instance_name
+    }
+  }
+
+
+  # elasticsearch::instance { 'es-01':
+  #   config => { },        # Configuration hash
+  #   init_defaults => { }, # Init defaults hash
+  # }
+
+
+  if $mount_snapshot {
+
+    package {'nfs-common':}
+
+    file {['/data',$snapshot_directory]:
+      ensure => directory,
+      mode   => '0777',
+    } ->
+
+    exec {"mount snapshot dir to ${snapshot_directory}":
+      command => "/bin/mount ${snapshot_server}:/data/snapshots ${snapshot_directory}",
+      unless  => "/bin/mount | grep ${snapshot_server} | grep ${snapshot_directory}",
+      require => Package['nfs-common']
+    }
+  }
 
 }
