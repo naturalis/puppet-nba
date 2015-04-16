af1114fef0e15f071844bf166c0f31cfe2ddb716
Atze de Vries <atze.devries@naturalis.nl>
2015-04-16 11:45:59 +0200

    added code for adding key

--- manifests/build.pp
+++ manifests/build.pp
@@ -3,6 +3,7 @@
 #
 class nba::build(
   $checkout,
+  $repokey,
 )
 {
   package {['git','ant','ivy','openjdk-7-jdk']:
@@ -12,6 +13,40 @@ class nba::build(
   file { '/etc/profile.d/ivy.sh':
     content => 'export IVY_HOME="/usr/share/maven-repo/org/apache/ivy/ivy/2.3.0/"'
   }
+
+  file { '/root/.ssh':
+    ensure    => directory,
+  }->
+# Create /root/.ssh/repokeyname file
+  file { '/root/.ssh/nbagit':
+    ensure  => present,
+    content => $repokey,
+    mode    => '0600',
+  }->
+# Create sshconfig file
+  file { '/root/.ssh/config':
+    ensure  => present,
+    content =>  "Host github.com\n\tIdentityFile ~/.ssh/nbagit",
+    mode    => '0600',
+  }->
+# copy known_hosts.sh file from puppet module
+  file{ '/usr/local/sbin/known_hosts.sh' :
+    ensure => present,
+    mode   => '0700',
+    source => 'puppet:///modules/nba/known_hosts.sh',
+  }->
+# run known_hosts.sh for future acceptance of github key
+  exec{ 'add_known_hosts' :
+    command  => '/usr/local/sbin/known_hosts.sh',
+    path     => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+    provider => shell,
+    user     => 'root',
+    unless   => 'test -f /root/.ssh/known_hosts'
+  }->
+# give known_hosts file the correct permissions
+  file{ '/root/.ssh/known_hosts':
+    mode      => '0600',
+  }->
 
   vcsrepo { '/opt/nba-git':
     ensure   => present,
@@ -29,6 +64,8 @@ class nba::build(
     command     => '/usr/bin/ant rebuild',
     refreshonly => true
   }
+
+
 
 
 
