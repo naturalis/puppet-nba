a6511a5ecc1811e554d732a6e62e7be417bf6d46
Atze de Vries <atze.devries@naturalis.nl>
2015-04-21 13:39:53 +0200

    uptade

--- manifests/import.pp
+++ manifests/import.pp
@@ -36,6 +36,6 @@ class nba::import()
   exec {'importit':
     command   => '/bin/mv /opt/boe/* /opt/data/ && /bin/echo "importing files"',
     logoutput => true,
-    unless    => '/usr/bin/lsof boe/*  2>&1 | grep "status\|COMMAND"'
+    unless    => '/usr/bin/lsof /opt/boe/*  2>&1 | grep "status\|COMMAND"'
   }
 }
