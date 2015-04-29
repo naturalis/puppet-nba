b04b13c028a0224d796ae555adb5aa7c2e8b04be
Atze de Vries <atze.devries@naturalis.nl>
2015-04-29 13:37:34 +0200

    dir structure

--- manifests/import.pp
+++ manifests/import.pp
@@ -9,4 +9,30 @@ class nba::import()
     logoutput => true,
     unless    => '/usr/bin/lsof /opt/boe/*  2>&1 | grep "status\|COMMAND"'
   }
+
+  file {[
+    '/data',
+    '/data/upload',
+    '/data/upload/crs',
+    '/data/upload/col',
+    '/data/upload/brahms',
+    '/data/upload/nsr',
+    '/data/import-logs']:
+    ensure => Directory,
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0770',
+    }
+
+    file {[
+      '/data/import',
+      '/data/import/crs',
+      '/data/import/col',
+      '/data/import/brahms',
+      '/data/import/nsr']:
+      ensure => Directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0700',
+    }
 }
