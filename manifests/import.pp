df946ac4de8e950a4b15ed3fcb1d4a039e4cfb57
Atze de Vries <atze.devries@naturalis.nl>
2015-04-30 09:56:12 +0200

    added check

--- manifests/import.pp
+++ manifests/import.pp
@@ -84,7 +84,7 @@ class nba::import()
     exec { 'take elasticsearch snapshot':
       command   => '/bin/echo taking snapshot tbi',
       logoutput => true,
-      unless    => ['/bin/ps aux | grep import','ls /var/run/nda-import.pid  2>&1 | grep cannot'],
+      unless    => ['/bin/ps aux | grep import','/bin/ls /var/run/nda-import.pid  2>&1 | grep cannot'],
     }
 
 }
