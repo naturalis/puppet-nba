32c52487e07c565fc011200a0a4b211a7e408b39
Atze de Vries <atze.devries@naturalis.nl>
2015-06-05 15:44:35 +0200

    added export function

--- manifests/roles/app.pp
+++ manifests/roles/app.pp
@@ -42,15 +42,15 @@ class  nba::roles::app (
     es_cluster_id => $elasticsearch_cluster_name,
     buildtype     => 'tag',
     build_ear     => true,
-    build_export  => false,
+    build_export  => true,
     deploy_ear    => true,
     main_es_ip    => $elasticsearch_ip_addresses,
     es_replicas   => 0,
     stage         => build,
   }
 
-  #class { 'nba::export':
-  #  version => $dcwa_eml_version,
-  #  stage   => main,
-  #}
+  class { 'nba::export':
+    version => $dcwa_eml_version,
+    stage   => main,
+  }
 }
