a89d9b82a79f47d575ba6bbe4e5986afacac92e6
Atze de Vries <atze.devries@naturalis.nl>
2015-07-31 09:53:58 +0200

    adg

--- manifests/roles/purl.pp
+++ manifests/roles/purl.pp
@@ -84,7 +84,7 @@ class  nba::roles::purl (
   exec {'create purl logger':
     cwd     => '/opt/wildfly/bin',
     command => './jboss-cli.sh -c command="/subsystem=logging/logger=nl.naturalis.purl:add(level=DEBUG)"',
-    unless  => './jboss-cli.sh -c command="ls subsystem=logging/logger" /bin/grep purl',
+    unless  => '/opt/wildfly/bin/jboss-cli.sh -c command="ls subsystem=logging/logger" /bin/grep purl',
     require => Class['wildfly'],
   }
 
