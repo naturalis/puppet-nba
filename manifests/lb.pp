bc1933a5846e3a089dcd8cb2f6d0590cc7f78c36
Atze de Vries <atze.devries@naturalis.nl>
2016-01-15 10:15:56 +0100

    dasgd

--- manifests/lb.pp
+++ manifests/lb.pp
@@ -13,9 +13,9 @@ class nba::lb (
 
   $log_default = { access_log => '/var/log/nginx/api.biodiversitydata.nl.access.log json'}
   class { '::nginx':
-    log_format => {
-      json => '{ "@timestamp": "$time_iso8601", "@fields": { "remote_addr": "$remote_addr", "remote_user": "$remote_user", "body_bytes_sent": "$body_bytes_sent", "request_time": "$request_time","status": "$status", "request": "$request", "request_method": "$request_method", "http_referrer": "$http_referer", "http_user_agent": "$http_user_agent" } }'
-    }
+     log_format => {
+       combined => '{ "@timestamp": "$time_iso8601", "@fields": { "remote_addr": "$remote_addr", "remote_user": "$remote_user", "body_bytes_sent": "$body_bytes_sent", "request_time": "$request_time","status": "$status", "request": "$request", "request_method": "$request_method", "http_referrer": "$http_referer", "http_user_agent": "$http_user_agent" } }'
+     }
   }
 
   #access_log
