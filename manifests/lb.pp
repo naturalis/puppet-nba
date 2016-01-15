914a1c57f89da7ee4db02a710d9b7675bf8d32a2
Atze de Vries <atze.devries@naturalis.nl>
2016-01-15 09:48:42 +0100

    dag

--- manifests/lb.pp
+++ manifests/lb.pp
@@ -11,13 +11,14 @@ class nba::lb (
   $files       = {}
 ){
 
+  $log_default = { access_log => '/var/log/nginx/api.biodiversitydata.nl.access.log json'}
   class { '::nginx':
     log_format => {
       json => '{ "@timestamp": "$time_iso8601", "@fields": { "remote_addr": "$remote_addr", "remote_user": "$remote_user", "body_bytes_sent": "$body_bytes_sent", "request_time": "$request_time","status": "$status", "request": "$request", "request_method": "$request_method", "http_referrer": "$http_referer", "http_user_agent": "$http_user_agent" } }'
     }
   }
 
-  access_log
+  #access_log
   # file { $directories :
   #   ensure => directory,
   # }
@@ -32,7 +33,7 @@ class nba::lb (
   # }
 
   create_resources(nba::lb::wwwfiles,$files,{})
-  create_resources(nginx::resource::vhost,$vhost,{ access_log => '/var/log/nginx/api.biodiversitydata.nl.access.log json'})
+  create_resources(nginx::resource::vhost,$vhost,$log_default)
   create_resources(nginx::resource::location,$location,{})
   create_resources(nginx::resource::upstream,$upstream,{})
 
