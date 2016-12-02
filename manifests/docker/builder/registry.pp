f94ea0e69bcd568849f00594f451aa6917fc5c92
Atze de Vries <atze.devries@naturalis.nl>
2016-12-02 10:28:51 +0100

    nicyfied reg

--- manifests/docker/builder/registry.pp
+++ manifests/docker/builder/registry.pp
@@ -1,19 +1,24 @@
 #
 #
 ### Creates local docker registry to server NBA images
-class nba::docker::builder::registry()
+class nba::docker::builder::registry(
+  $registry_name = 'nba-registry',
+  $listen_port   = '5000',
+  )
 {
   include 'docker'
 
-  file { '/nba-registry' :
+  file { ["/${registry_name}","/var/log/docker-${registry_name}"] :
     ensure => directory,
   }
 
-  docker::run {'nba-registry':
+  docker::run {$registry_name:
     image   => 'registry:2',
-    ports   => '5000:5000',
-    volumes => ['/nba-registry:/var/lib/registry'],
-    require => File['/nba-registry'],
+    ports   => "${listen_port}:5000",
+    volumes => ["/${registry_name}:/var/lib/registry",
+                "/var/log/docker-${registry_name}:/var/log"],
+    require => [File["/${registry_name}"],
+                File["/var/log/docker-${registry_name}"]],
   }
 
 }
