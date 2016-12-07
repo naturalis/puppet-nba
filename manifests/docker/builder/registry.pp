f2d7c2b957f4aa93eabd5ef8e6ccb67e9533a6d8
Atze de Vries <atze.devries@naturalis.nl>
2016-12-07 11:54:16 +0100

    dagdg

--- manifests/docker/builder/registry.pp
+++ manifests/docker/builder/registry.pp
@@ -6,6 +6,7 @@ class nba::docker::builder::registry(
   $listen_port   = '5000',
   )
 {
+  #todo fix ssl!
   include 'docker'
 
   file { ["/${registry_name}","/var/log/docker-${registry_name}","/${registry_name}-certs"] :
@@ -18,7 +19,7 @@ class nba::docker::builder::registry(
     volumes          => ["/${registry_name}:/var/lib/registry",
                         "/var/log/docker-${registry_name}:/var/log",
                         "/${registry_name}-certs:/certs"],
-    env              => ['REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt','REGISTRY_HTTP_TLS_KEY=/certs/domain.key']
+    env              => ['REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt','REGISTRY_HTTP_TLS_KEY=/certs/domain.key'],
     extra_parameters => [ '--restart=always' ],
     require          => [File["/${registry_name}"],
                         File["/var/log/docker-${registry_name}"]],
